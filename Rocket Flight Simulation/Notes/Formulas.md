1e55a51518cbd65d50fd45fe82b5fe54fd87f914
Gomes0312 <drpedrogois@hotmail.com>
2026-06-23 16:37:30 -0300

    Add files via upload

---
+++ Rocket Flight Simulation/Notes/Formulas.md
@@ -0,0 +1,25 @@
+Resultant Force:
+
+Fr = T - (Da + Dc + P)
+
+a = (T - D - P) / m
+
+T = Thrust
+Da = Aerodynamica cone DRAG
+Dc = Capsule DRAG
+P = Weight Force
+
+Drag:
+
+D = 1/2 p v^2 Dc A
+
+D = (Dc + Da)
+
+p = Air pressure
+v = Speed
+Dc = DRAG Coefficient
+A = Area
+
+Weight Force:
+
+P = mg
---
+++ Rocket Flight Simulation/Simulations/Test1.py
@@ -0,0 +1,27 @@
+import math
+
+#Data
+m = 2350
+g = 9.81
+T = 163000
+Da = 0.37
+Dc = 0.28
+p = 1.225
+Aa = 0.3
+Ac = 0.91
+dt = 0.5
+D = (Dc + Da)
+P = m*g
+
+h = 0
+v = 0
+t = 0
+
+for i in range (18):
+a = (T - D - P) / m
+Dc = 0.5 * p * v**2 * Dc * Ac
+Da = 0.5 * p * v**2 * Da * Aa
+t = t+dt
+v = v+a*dt
+h = h+v*dt
+print (h,v,t)
