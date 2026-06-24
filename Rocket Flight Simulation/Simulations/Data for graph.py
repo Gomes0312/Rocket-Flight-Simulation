8fe84a05eaa307a3a81e635162a31afca9a2877f
Gomes0312 <drpedrogois@hotmail.com>
2026-06-23 23:14:17 -0300

    Create Data for graph.py

---
+++ Rocket Flight Simulation/Simulations/Data for graph.py
@@ -0,0 +1,32 @@
+import math
+
+#Data
+m = 2350
+g = 9.81
+T = 163000
+Da = 0.37
+Dc = 0.28
+p = 1.225
+Aa = 0.3
+Ac = 0.91
+dt = 0.05
+D = (Dc + Da)
+P = m*g
+h = 0
+v = 0
+t = 0
+altitudes = []
+times = []
+
+for i in range (177):
+a = (T - D - P) / m
+Dc = 0.5 * p * v**2 * Dc * Ac
+Da = 0.5 * p * v**2 * Da * Aa
+t = t+dt
+v = v+a*dt
+h = h+v*dt
+altitudes.append(h)
+times.append(t)
+
+print (altitudes, times)
+#delete times or altitudes depending what i want for the graph
