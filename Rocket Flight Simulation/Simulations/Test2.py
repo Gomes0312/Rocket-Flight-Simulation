fbab56b48c13bf81a59e723e9bee7d8569efb643
Gomes0312 <drpedrogois@hotmail.com>
2026-06-23 21:13:45 -0300

    Update Test2.py

--- Rocket Flight Simulation/Simulations/Test2.py
+++ Rocket Flight Simulation/Simulations/Test2.py
@@ -1,1 +1,32 @@
+import math
 
+#Data
+m = 2350
+T = 163000
+Da = 0.37
+Dc = 0.28
+p = 1.225
+Aa = 0.3
+Ac = 0.91
+dt = 0.05
+G = 6.67 * 10**-11
+R = 6378137
+M = 5.97 * 10**24
+h = 0
+v = 0
+t = 0
+
+#formulas
+D = (Dc + Da)
+P = m*g
+g = G * M / (h + R)**2
+
+for i in range (177):
+a = (T - D - P) / m
+Dc = 0.5 * p * v**2 * Dc * Ac
+Da = 0.5 * p * v**2 * Da * Aa
+t = t+dt
+v = v+a*dt
+h = h+v*dt
+
+print (h,v,t)
