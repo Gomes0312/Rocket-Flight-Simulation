5167e9d72fecc6366aacf3be385a5b54fc85f848
Gomes0312 <drpedrogois@hotmail.com>
2026-06-26 11:55:05 -0300

    Update Level1.py

--- Rocket Flight Simulation/Simulations/Level1.py
+++ Rocket Flight Simulation/Simulations/Level1.py
@@ -12,11 +12,13 @@ Ac = 0.91
 dt = 0.05
 D = (Dc + Da)
 P = m*g
-
+#Initial Values
 h = 0
 v = 0
 t = 0
-
+MaxSpeed = v
+MaxSpeedAltitude = h
+#With Propulsion
 for i in range (177):
 a = (T - D - P) / m
 Dc = 0.5 * p * v**2 * Dc * Ac
@@ -24,5 +26,17 @@ Da = 0.5 * p * v**2 * Da * Aa
 t = t+dt
 v = v+a*dt
 h = h+v*dt
+if v > MaxSpeed:
+MaxSpeed = v
+MaxSpeedAltitude = h
 
-print(f"Max altitude = {h:.2f} meters | Max speed = {v:.2f}m/s")
+#No Propulsion
+while (v>0):
+a = (- D - P) / m
+Dc = 0.5 * p * v**2 * Dc * Ac
+Da = 0.5 * p * v**2 * Da * Aa
+t = t+dt
+v = v+a*dt
+h = h+v*dt
+
+print(f"Max altitude = {h:.2f} meters | Max speed = {MaxSpeed:.2f}m/s at {MaxSpeedAltitude:.2f} meters | Time of flight = {t:.2f} seconds")
