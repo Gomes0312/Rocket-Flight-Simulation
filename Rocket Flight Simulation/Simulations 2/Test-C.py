f3ddd6ca8c2e66b25a9999b855564505734f0611
Gomes0312 <drpedrogois@hotmail.com>
2026-08-26 20:49:54 -0300

    Update Test-C.py

--- Rocket Flight Simulation/Simulations 2/Test-C.py
+++ Rocket Flight Simulation/Simulations 2/Test-C.py
@@ -1,1 +1,20 @@
+import math
+#test for variable gravity
+dt = 0.001
+Bt = 9.999 #Tempo de queima
+R = 6*10**5
+g = 9.81
+#Initial values:
+v = 100
+h = t = vi = vf = vm = 0
 
+while (v>0):
+h_rel = h/R
+vi = v
+v = v-(a*dt)
+vf = v
+vm = (vi+vf)/2
+h = h+(vm*dt)
+t = t+dt
+
+print(v,t,h)
