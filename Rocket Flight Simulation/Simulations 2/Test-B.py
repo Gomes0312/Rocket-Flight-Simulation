5bcf74eaba560fc770680c079428b3ee226f244a
Gomes0312 <drpedrogois@hotmail.com>
2026-08-26 19:04:01 -0300

    Update Test-B.py

--- Rocket Flight Simulation/Simulations 2/Test-B.py
+++ Rocket Flight Simulation/Simulations 2/Test-B.py
@@ -1,1 +1,18 @@
+import math
+#test for constant gravity
+dt = 0.001
+a = g = 10
+Bt = 9.999 #Tempo de queima
+#Initial values:
+v = 100
+h = t = vi = vf = vm = 0
 
+while (v>0):
+vi = v
+v = v-(a*dt)
+vf = v
+vm = (vi+vf)/2
+h = h+(vm*dt)
+t = t+dt
+
+print(v,t,h)
